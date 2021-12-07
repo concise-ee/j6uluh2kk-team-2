8f3963f0b8408b6c2c3da5750695db979f8f808c
alekseiodegov <aleksei.odegov@easypark.net>
2021-12-07 15:29:28 +0200

    js

--- api/server.js
+++ api/server.js
@@ -4,31 +4,13 @@ const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
 
-// place holder for the data
 const users = [];
 
-// app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, './')));
-//
-// app.get('/api/christmas', (req, res) => {
-//   console.log('merry called!')
-//   res.json({
-//     isItChristmasYet: false
-//   });
-// });
-// app.get('/js', express.static(__dirname + '/js'));
-// app.get('/img', express.static(__dirname + '/img'));
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
-app.use(express.static('js'));
-app.use(express.static('img'));
-//
-// app.get('/img/cat.svg', (req,res) => {
-//     res.sendFile(path.join(__dirname, './img/cat.svg'));
-// });
-
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
